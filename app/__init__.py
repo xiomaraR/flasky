637750222328b0bac61e79505d90360f5698e203
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2022-04-22 13:31:16 -0700

    Building an API, Part 1

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
+    from .routes.routes import bp
+
+    app.register_blueprint(bp)
+
     return app

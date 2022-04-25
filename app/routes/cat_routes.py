9741b71e082904221ead6c2f32408e3a0370d093
Ashley Yang <ashley.minghui@gmail.com>
2022-04-25 11:32:31 -0700

    2) Building an API - Read one cat and error handling

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,17 +1,18 @@
-from tkinter import N
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, abort 
 
 class Cat:
-    def __init__(self, name, personality, breed, age, tricks):
+    def __init__(self, id, name, personality, breed, age, tricks=None):
+        self.id = id
         self.name = name
         self.personality = personality
         self.breed = breed
         self.age = age
-        self.tricks = tricks
+        self.tricks = tricks or []
         self.toe_beans = 16
 
     def to_json(self): #this method can be used across any route/function
         return {
+            "id": self.id,
             "name": self.name,
             "personality": self.personality,
             "breed": self.breed,
@@ -21,10 +22,10 @@ class Cat:
         }
 
 cats = [
-    Cat("hamburger", "sassy", "tabby", 4, ["loves Philomena"]),
-    Cat("Scully", "grumpy", "tuxdedo", 4, []),
-    Cat("Leonardo Dicatrio", "cuddly", "tuxdedo", 4, []),
-    Cat("Luna", "pragmatic", 100, ["magic"])
+    Cat(1, "hamburger", "sassy", "tabby", 4, ["loves Philomena"]),
+    Cat(2, "Scully", "grumpy", "tuxdedo", 4, []),
+    Cat(3, "Leonardo Dicatrio", "cuddly", "tuxdedo", 4, []),
+    Cat(4, "Luna", "pragmatic", "scottish fold", 100, ["magic"])
 ]
 
 cat_bp = Blueprint("cat", __name__, url_prefix="/cats")
@@ -48,3 +49,21 @@ def read_all_cats():
         # })
 
     return jsonify(cats_response)
+
+def validate_cat(id):
+    try:
+        id = int(id)
+    except:
+        return abort(make_response({"message": f"cat {id} is invalid"}, 400))
+
+    for cat in cats:
+        if cat.id == id:
+            return cat
+
+    return abort(make_response({"message":f"cat {id} not found"}, 404))
+
+# GET one cat
+@cat_bp.route("/<id>", methods = ["GET"])
+def read_one_cat(id):
+    cat = validate_cat(id)
+    return jsonify(cat.to_json(), 200)
